7dd9bdb167fe91997f77c2fa9a0407ea8788e2b9
mruiz723 <mruiz723@gmail.com>
2016-04-07 21:57:26 -0500

    Add ViewControllers with their classes view controllers

---
+++ iOSControls/iOSControls/ScrollViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ScrollViewController.swift
+//  iOSControls
+//
+//  Created by mruiz723 on 4/7/16.
+//  Copyright © 2016 Udem. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- iOSControls/iOSControls/ViewController.swift
+++ iOSControls/iOSControls/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var controlsTableView: UITableView!
+    
+    
+    //MARK: - Properties
+    var menuItems:[String]?
+    
+    
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        menuItems = ["Toolbar", "ImageView", "Pickers", "Slider", "Switch", "CustomFonts", "ScrollView"]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    //MARK: - UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (menuItems?.count)!
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if let cell = controlsTableView.dequeueReusableCellWithIdentifier("cell") {
+            cell.textLabel?.text = menuItems![indexPath.row]
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    //MARK: - UITableViewDelegate
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.performSegueWithIdentifier(menuItems![indexPath.row], sender: nil)
+    }
+    
 
 }
 
