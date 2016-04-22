57eb90372a099511443deade762dc668a1a1e99a
mruiz723 <mruiz723@gmail.com>
2016-04-21 22:27:58 -0500

    Add Podfile and Alamofire.

---
+++ iOSControls/Pods/Target Support Files/Pods-iOSControls/Pods-iOSControls-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSControls : NSObject
+@end
+@implementation PodsDummy_Pods_iOSControls
+@end
---
+++ iOSControls/Pods/Target Support Files/Pods-iOSControls/Pods-iOSControls-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_iOSControlsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_iOSControlsVersionString[];
+
