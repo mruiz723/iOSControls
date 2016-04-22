0615c5b6f048418e36cb9f133532ce6b8e0ab0c2
mruiz723 <mruiz723@gmail.com>
2016-04-21 23:10:21 -0500

    Add Contact model and Request.

---
+++ iOSControls/iOSControls/Models/Contact.swift
@@ -0,0 +1,133 @@
+//
+//  Contacts.swift
+//  ExampleAlamofirePUTDELETE
+//
+//  Created by Laura Mejia Arroyave on 2/24/16.
+//  Copyright © 2016 NextU. All rights reserved.
+//
+
+import UIKit
+
+typealias CompletionHandler = (success: Bool, response: [Contact]) ->()
+
+class Contact {
+    
+    let request: Request = Request()
+    
+    var contacts = [Contact]()
+    
+    //MARK: - Properties
+    var id: String?
+    var name: String?
+    var lastName: String?
+    var phone: String?
+    var email: String?
+    
+    
+    //MARK: - Init
+    init(id: String, name:String, lastName:String, phone:String, email:String){
+        self.id = id
+        self.name = name
+        self.lastName = lastName
+        self.phone = phone
+        self.email = email
+    }
+    
+    convenience init(){
+        
+        self.init(id:"", name:"", lastName:"", phone:"", email:"")
+        
+    }
+    
+    func getContacts(completion: CompletionHandler){
+        
+        request.getContacts { (success, response) -> () in
+            
+            if success{
+                
+                self.contacts.removeAll()
+                
+                for item in response{
+                    
+                    let contact = Contact(id: item["_id"] as? String ?? "",
+                                        name: item["name"] as? String ?? "",
+                                        lastName: item["last_name"] as? String ?? "",
+                                        phone: item["phone"] as? String ?? "",
+                                        email: item["email"] as? String ?? "")
+                    
+                    self.contacts.append(contact)
+                }
+                
+                completion(success: true, response: self.contacts)
+                
+            }else{
+                
+                completion(success: false, response: self.contacts)
+            }
+            
+        }
+    }
+    
+    func saveContact(contact: Contact, completion:CompletionHandler){
+        let parameters : [String: AnyObject] = ["name": contact.name!, "last_name": contact.lastName!, "phone":contact.phone!, "email":contact.email!]
+        
+        request.saveContact(parameters) { (success, response) -> () in
+            if success{
+                self.contacts.append(contact)
+                completion(success: true, response: self.contacts)
+            }else{
+                completion(success: false, response: self.contacts)
+            }
+            
+        }
+    }
+    
+    func deleteContact(index : Int, contactID: String, completion: CompletionHandler){
+        
+        request.deleteContact(contactID) { (success, response) -> () in
+            
+            if success{
+                
+                self.contacts.removeAtIndex(index)
+                completion(success: true, response: self.contacts)
+                
+            }else{
+                
+                completion(success: false, response: self.contacts)
+            }
+            
+        }
+    }
+    
+    func updateContact(contact : Contact, completion: CompletionHandler){
+        
+        
+        let parameters : [String: AnyObject] = ["name": contact.name!, "last_name": contact.lastName!, "phone":contact.phone!, "email":contact.email!]
+        
+        request.updateContact(contact.id!, parameters: parameters) { (success, response) -> () in
+            
+            if success{
+                
+                for item in self.contacts{
+                    
+                    if item.id == contact.id!{
+                        
+                        item.name = contact.name
+                        item.lastName = contact.lastName
+                        item.phone = contact.phone
+                        item.email = contact.email
+                    }
+                }
+                
+                completion(success: true, response: self.contacts)
+                
+            }else{
+                
+                completion(success: false, response: self.contacts)
+            }
+            
+        }
+    }
+}
+
+ 
---
+++ iOSControls/iOSControls/Utils/Request.swift
@@ -0,0 +1,90 @@
+//
+//  Request.swift
+//  ExampleAlamofirePUTDELETE
+//
+//  Created by Laura Mejia Arroyave on 2/23/16.
+//  Copyright © 2016 NextU. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+public typealias CompletionHandler =  (success: Bool, response: [String : AnyObject]) ->()
+
+let headers = [
+    "Authorization": "Basic a2lkX1cxQmtMTUhEQ3g6ZWJiZjVhODg0MGIxNDg5NWFlOTg3YzM3MjIxZDE5NGE=",
+    "Content-Type": "application/x-www-form-urlencoded"
+]
+
+class Request: NSObject{
+    
+    let url = "https://baas.kinvey.com/appdata/kid_W1BkLMHDCx/Contacts/"
+    
+    func getContacts(completion:CompletionHandler){
+        Alamofire.request(.GET, url, parameters: nil, headers: headers)
+            .responseJSON(){response in
+                
+                switch response.result {
+                case .Success(let JSON):
+                    print("Llamado de GET \(JSON)")
+                    completion(success: true, response: JSON as! [[String : AnyObject]])
+                case .Failure(let error):
+                    completion(success: false, response: [["error":error.localizedDescription]])
+                }
+        }
+    }
+    
+    func saveContact(parameters:[String: AnyObject], completion:CompletionHandler){
+        Alamofire.request(.POST, url, parameters:parameters, headers:headers)
+            .responseJSON(){response in
+            switch response.result {
+            case .Success(let JSON):
+                completion(success: true, response: JSON as! [String : AnyObject])
+            case .Failure(let error):
+                completion(success: false, response: ["error":error.localizedDescription])
+            }
+        }
+    }
+    
+    func deleteContact(contactID: String, completion:CompletionHandler){
+        
+        let urlWithContact : String = "\(url)\(contactID)"
+        
+        Alamofire.request(.DELETE, urlWithContact, parameters: nil, headers: headers)
+            .responseJSON(){response in
+                
+                switch response.result {
+                    
+                case .Success(let JSON):
+                    
+                    print("Borrado 1 contacto")
+                    completion(success: true, response: ["response":JSON])
+                    
+                case .Failure(let error):
+                    
+                    completion(success: false, response: ["error":error.localizedDescription])
+                }
+        }
+    }
+    
+    func updateContact(contactID: String, parameters: [String: AnyObject], completion:CompletionHandler){
+        
+        let urlWithContact : String = "\(url)\(contactID)"
+        
+        Alamofire.request(.PUT, urlWithContact, parameters: parameters, headers: headers)
+            .responseJSON(){response in
+                
+                switch response.result {
+                case .Success(let JSON):
+                    
+                    print("Actualizado 1 contacto")
+                    completion(success: true, response: ["response":JSON])
+                    
+                case .Failure(let error):
+                    
+                    completion(success: false, response: ["error":error.localizedDescription])
+                }
+                
+        }
+    }
+}
